583bd2887e731ebf79eb14c024630587ad167a6a
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-01-29 20:20:29 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,8 +16,9 @@ class CashRegister
       @discount = @discount/100.to_f
       @amount_to_takeoff = (@total * @discount)
       @total = @total - @amount_to_takeoff
-      puts "After the discount, the total comes to $#{@total.to_i}"
-    end
+      return "After the discount, the total comes to $#{@total}"
+    else
+      return "There is no discount to apply."
   end
   
   
