17ea687d69d0c2155e0a517ade5aeb384dc57989
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-01-29 20:19:02 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,7 @@ class CashRegister
   end
   
   def apply_discount
-    if @discount != 0
+    if @discount > 0
       @discount = @discount/100.to_f
       @amount_to_takeoff = (@total * @discount)
       @total = @total - @amount_to_takeoff
